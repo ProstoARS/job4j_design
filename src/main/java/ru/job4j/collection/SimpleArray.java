f519feea5bace68ce0d674b4152a76204cef6497
ProstoARS <arsudakov@gmail.com>
2021-06-14 17:58:21 +0300

    1. Динамический список на массиве. [#158 #166878]

--- src/main/java/ru/job4j/collection/SimpleArray.java
+++ src/main/java/ru/job4j/collection/SimpleArray.java
@@ -1,15 +1,14 @@
 package ru.job4j.collection;
 
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-import java.util.Objects;
+import java.util.*;
 
 public class SimpleArray<T> implements Iterable<T> {
 
     private T[] data;
 
     private int pointer = 0;
+
+    private int modCount = 0;
 
     SimpleArray() {
         data = (T[]) new Object[1];
@@ -23,11 +22,11 @@ public class SimpleArray<T> implements Iterable<T> {
     public void add(T model) {
         if (pointer == data.length) {
             T[] temp = data;
-            data = (T[]) new Object[temp.length + 1];
-            System.arraycopy(temp, 0, data, 0, temp.length);
+            data = Arrays.copyOf(temp, temp.length * 2);
         }
         data[pointer] = model;
         pointer++;
+        modCount++;
     }
 
     @Override
@@ -38,19 +37,19 @@ public class SimpleArray<T> implements Iterable<T> {
     private class SimpleIterator<E> implements Iterator<E> {
         int index;
         E[] data;
-        private int modCount = 0;
+        private int expectedModCount = 0;
 
         public SimpleIterator(E[] data, int pointer) {
             this.data = data;
-            this.modCount = pointer;
+            this.expectedModCount = pointer;
         }
 
         @Override
         public boolean hasNext() {
-            if (pointer != modCount) {
+            if (modCount != expectedModCount) {
                 throw new ConcurrentModificationException();
             }
-            return index < modCount;
+            return index < pointer;
         }
 
         @Override
