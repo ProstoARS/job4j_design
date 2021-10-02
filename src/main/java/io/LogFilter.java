499b1495ef8c48c80bc53182dec87bc9f72262d1
ProstoARS <arsudakov@gmail.com>
2021-10-02 21:43:19 +0300

    0.3. BufferedReader. [#252489]

---
+++ src/main/java/io/LogFilter.java
@@ -0,0 +1,26 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LogFilter {
+    public static List<String> filter(String file) {
+        List<String> list = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            list = in.lines()
+                    .filter(line -> line.contains("404") && !line.endsWith("-"))
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        List<String> log = filter("log.txt");
+        System.out.println(log);
+    }
+}
