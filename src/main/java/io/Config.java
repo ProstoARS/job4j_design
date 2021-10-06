4706ecadb9a0f22e67fceed98dbfca00e62890cb
ProstoARS <arsudakov@gmail.com>
2021-10-06 16:38:56 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/io/Config.java
@@ -0,0 +1,63 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
+public class Config {
+
+    private final String path;
+    private Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            values = read.lines()
+                    .filter(a -> !a.startsWith("#") && !a.isEmpty())
+                    .collect(Collectors.toMap(a -> {
+                        String[] subStr = a.split("=");
+                        if (subStr[0] == null || subStr[0].equals(" ")) {
+                            throw new IllegalArgumentException();
+                        }
+                        return subStr[0];
+                    }, a -> {
+                        String[] subStr = a.split("=");
+                        return subStr[1];
+                    }));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        String str = values.get(key);
+        if (str == null) {
+            throw new UnsupportedOperationException("Don't impl this method yet!");
+        }
+       return str;
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+
+}
---
+++ src/test/java/io/ConfigTest.java
@@ -0,0 +1,31 @@
+package io;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "C:\\IdeaProjects\\job4j_design\\data\\pair_without_comment.properties.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Arseny Sudakov"));
+    }
+
+    @Test
+    public void whenPairWithoutCommentAndEmtyLine() {
+        String path = "C:\\IdeaProjects\\job4j_design\\data\\pairWithoutCommentAndEmtyLine.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Arseny Sudakov"));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenPairViolation() {
+        String path = "C:\\IdeaProjects\\job4j_design\\data\\pairViolation.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+}
