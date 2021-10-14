9c95cbc570dc88bbcf8c1a8ba3f496d1f4e9eca3
ProstoARS <arsudakov@gmail.com>
2021-10-14 20:29:00 +0300

    4.0. File [#252491]

---
+++ src/main/java/io/Dir.java
@@ -0,0 +1,20 @@
+package io;
+
+import java.io.File;
+import java.util.Objects;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("c:\\IdeaProjects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.printf("Total disk size : %s Gb%n", file.getTotalSpace() / 1073741824);
+        for (File subfile : Objects.requireNonNull(file.listFiles())) {
+            System.out.println(subfile.getName() + " : " + subfile.length() / 1024 + "Kb");
+        }
+    }
+}
