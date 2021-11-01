33ae489adbcbdf228c20412221d084d1e2e2af98
ProstoARS <arsudakov@gmail.com>
2021-11-01 09:48:59 +0300

    5.2. Архивировать проект [#861 #222574]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -23,35 +23,35 @@ public class Zip {
         }
     }
 
-    public static void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        packSingleFile(
-                new File("./pom.xml"),
-                new File("./pom.zip")
-        );
+    public static void validation(String[] args) {
         if (args.length != 3) {
             throw new IllegalArgumentException("Тot enough arguments. Please enter: -d=ROOT_FOLDER -e=EXCLUDE_FILE"
                     + " -o=TARGET_DIR");
         }
-        ArgsName an = ArgsName.of(args);
-        Path source = Paths.get(an.get("d"));
-        if (!source.toFile().exists()) {
+    }
+
+    public static void validArgs(Path source, String extension, File output) {
+        if (!source.toFile().exists() && !source.toFile().isDirectory()) {
             throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar -d=ROOT_FOLDER.");
         }
-        List<Path> filesWithOutUnnecessary = Search.search(source,
-                p -> !p.toFile().getName().endsWith(an.get("e")));
-        List<File> filesToArchive = filesWithOutUnnecessary.stream().map(Path::toFile).collect(Collectors.toList());
+        if (extension == null) {
+            throw new IllegalArgumentException("enter file extension to exclude from archive: -e=EXTENSION");
+        }
+        if (output == null) {
+            throw new IllegalArgumentException("specify the target path for the archive: -o=TARGET");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        validation(args);
+        ArgsName an = ArgsName.of(args);
+        Path source = Paths.get(an.get("d"));
         File output = new File(an.get("o"));
+        String extension = an.get("e");
+        validArgs(source, extension, output);
+        List<Path> filesWithOutUnnecessary = Search.search(source,
+                p -> !p.toFile().getName().endsWith(extension));
+        List<File> filesToArchive = filesWithOutUnnecessary.stream().map(Path::toFile).collect(Collectors.toList());
         packFiles(filesToArchive, output);
     }
 }
