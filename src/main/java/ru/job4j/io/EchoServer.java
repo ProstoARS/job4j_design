15ba3811a56fef2eaab84e6a2ebd36cc247299c3
ProstoARS <arsudakov@gmail.com>
2021-11-09 20:40:11 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -7,19 +7,31 @@ import java.net.Socket;
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
+            int count = 0;
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    if (count == 0) {
+                        out.write("Hello, dear friend.\r\n".getBytes());
+                    }
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
-                        if (str.contains("?msg=Bye")) {
-                            server.close();
+                        if (str.contains("?msg=")) {
+                            if (str.contains("Hello")) {
+                                out.write("Hello=).\r\n".getBytes());
+                            } else if (str.contains("Exit")) {
+                                out.write("server close".getBytes());
+                                server.close();
+                            } else {
+                                out.write("What=(.\r\n".getBytes());
+                            }
                         }
+                        out.flush();
                     }
-                    out.flush();
+                    count++;
                 }
             }
             System.out.println("server close");
