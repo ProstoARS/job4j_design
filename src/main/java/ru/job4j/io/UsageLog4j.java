1356cc134b1af38a24fed55a7d19b8e0f7b155c2
ProstoARS <arsudakov@gmail.com>
2021-11-10 14:27:22 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,16 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Arseny Sudakov";
+        byte number = 77;
+        int age = 33;
+        char league = 'f';
+        float averagePoints = 3.2f;
+        double efficiency = 34.8;
+        boolean sex = true;
+        long time = 2345678;
+        LOG.debug("User info name : {}, age : {}, number : {}, league : {}, averagePoints : {},"
+                + " efficiency : {}, sex : {}, time : {}.", name, age, number, league,
+                averagePoints, efficiency, sex, time);
     }
 }
