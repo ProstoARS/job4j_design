ff7966dbccbd1c4bbaeb63ebc3f2aeae243ea564
ProstoARS <arsudakov@gmail.com>
2021-12-08 11:07:38 +0300

    2. Поиск файлов по критерию [#783 #237192]

--- src/main/java/ru/job4j/io/find/TypeFinderSwitch.java
+++ src/main/java/ru/job4j/io/find/TypeFinderSwitch.java
@@ -5,8 +5,8 @@ import ru.job4j.io.Search;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -25,17 +25,27 @@ public class TypeFinderSwitch {
     }
 
     public List<File> get() throws IOException {
-        List<Path> temp = new ArrayList<>();
+        List<Path> temp;
         switch (arg) {
             case "name":
                 temp = Search.search(source, path -> path.toFile().getName().equals(attribute));
                 break;
             case "mask":
-                System.out.println("тут пока не знаю что делать");
+                String maskToRegex = attribute;
+                maskToRegex = maskToRegex.replaceAll("\\*", "\\.\\*");
+                maskToRegex = maskToRegex.replaceAll("\\?", "\\.\\{1\\}");
+                Pattern patt = Pattern.compile(maskToRegex);
+                temp = Search.search(source, path -> {
+                    Matcher matcher = patt.matcher(path.toFile().getName());
+                    return matcher.matches();
+                });
                 break;
             case "regex":
                 Pattern pattern = Pattern.compile(attribute);
-                temp = Search.search(source, path -> path.toFile().getName().matches(pattern.toString()));
+                temp = Search.search(source, path -> {
+                   Matcher matcher = pattern.matcher(path.toFile().getName());
+                   return matcher.matches();
+                });
                 break;
             default:
                 System.out.println("wrong argument -t. Please enter: \"name\", \"mask\", \"regex\"");
